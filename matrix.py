5eb176d87b23cb02ad5e80c76f0fc381b08e9128
timothylu <timothy.e.lu@gmail.com>
2017-08-29 08:18:30 -0700

    Add files via upload

--- matrix.py
+++ matrix.py
@@ -1,37 +1,76 @@
 import sys
 import numpy as np
 from random import randint
+import minimum
 
 def read_matrix_file(filename):
     matrix = []
-    with open(filename) as file:
-        for i, line in enumerate(file):
-            temp = []
-            temp = line.split()
-            matrix.append(temp)
-            temp[:] = []
-    return np.matrix(np.array(matrix))
+    print("reading...")
+
+    try:
+        with open(filename) as file:
+            for i, line in enumerate(file):
+                temp = []
+                temp = line.split()
+                temp = [int(i) for i in temp]
+                #print(temp)
+                matrix.append(temp[:])
+                temp[:] = []
+        return_mat = np.matrix(np.array(matrix))
+        print(return_mat)
+        print("# of rows: " + str(return_mat.shape[0]))
+        print()
+        return return_mat
+    except:
+    	print("ERROR: could not find " + filename)
+    	sys.exit
 
 def rand_rows(num, matrix):
+    print("randomizing...")
     rows = set()
     for i in range(0, num):
-        current = randint(0, matrix.size)
+        current = randint(0, matrix.shape[0] - 1)
         while(current in rows):
-            current = randint(0, matrix.size)
+            current = randint(0, matrix.shape[0] - 1)
         rows.add(current)
 
     temp = []
     for row in rows:
         temp.append(matrix[row])
-    return np.matrix(np.array(temp))
+    return_mat = np.matrix(np.array(temp))
+    print (return_mat)
+    print("# of rows: " + str(return_mat.shape[0]))
+    print()
+    return return_mat
 
 def generate_paired_matrix(matrix):
+    print("generating paired matrix...")
     temp = []
-    for row in matrix:
-        for row1 in matrix:
-            if row not in row1:
-                temp.append(row + row1)
-    return np.matrix(np.array(temp))
+    for i in range(0, matrix.shape[0]):
+        for j in range(0, matrix.shape[0]):
+            if not np.array_equal(matrix[i], matrix[j]):
+                #print("summing: " + str(matrix[i]) + str(matrix[j]) + "...")
+                temp.append(np.sum([matrix[i], matrix[j]], 0))
+            else:
+                #print("row " + str(i) + " and row " + str(j) + " are equal." + str(matrix[i]) + str(matrix[j])) 
+                pass
+    return_mat = np.matrix(np.array(temp))
+    print(return_mat)
+    print("# of rows: " + str(return_mat.shape[0]))
+    print()
+    return return_mat
 
 def main():
-    generate_paired_matrix(rand_rows(read_matrix_file("input.txt")))
+    inp = read_matrix_file("input.txt")
+    rand = rand_rows(128, inp)
+    mat = generate_paired_matrix(rand)
+    out = open('output.txt', 'w+')
+    out.close()
+    print(minimum.min_row(mat))
+    print(minimum.min_row(mat).sum())
+    # with open('output.txt') as f:
+    # 	for line in mat:
+    #     	np.savetxt(f, line, fmt='%.18e')
+
+if __name__ == "__main__":
+    main()
---
+++ minimum.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def min_row(mat):
+	min_row = mat[0]
+	#print("first row sum: " + str(min_row.sum()))
+	for row in mat:
+		temp = row.sum()
+		if temp < min_row.sum():
+			#print("new min: " + str(temp))
+			min_row = row
+	return min_row
+
+if __name__ == "__main__":
+	pass
