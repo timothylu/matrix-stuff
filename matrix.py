2cec3d48567f3958e3e186e2113e9e788e9540a4
timothylu <timothy.e.lu@gmail.com>
2017-08-29 18:49:43 -0700

    can find the indexes of all rows relative to the original matrix

--- matrix.py
+++ matrix.py
@@ -33,13 +33,15 @@ def read_matrix_file(filename):
 
 # Returns a matrix with "num" number of rows picked randomly from "matrix"
 def rand_rows(num, matrix):
-    print("randomizing...")
+    print("randomizing to " + str(num)+ " rows...")
+    idx = []
     rows = set()
     for i in range(0, num):
         current = randint(0, matrix.shape[0] - 1)
         while(current in rows):
             current = randint(0, matrix.shape[0] - 1)
         rows.add(current)
+        idx.append(i)
 
     temp = []
     for row in rows:
@@ -48,17 +50,19 @@ def rand_rows(num, matrix):
     print (return_mat)
     print("# of rows: " + str(return_mat.shape[0]))
     print()
-    return return_mat
+    return idx, return_mat
 
 # Generates a paired matrix (adds every row in "matrix" with every other row)
-def generate_paired_matrix(matrix):
+def generate_paired_matrix(idx, matrix):
     print("generating paired matrix...")
     temp = []
+    idx_return = []
     for i in range(0, matrix.shape[0]):
         for j in range(0, matrix.shape[0]):
             if not np.array_equal(matrix[i], matrix[j]):
                 #print("summing: " + str(matrix[i]) + str(matrix[j]) + "...")
                 temp.append(np.sum([matrix[i], matrix[j]], 0))
+                idx_return.append([idx[i], idx[j]])
             else:
                 #print("row " + str(i) + " and row " + str(j) + " are equal." + str(matrix[i]) + str(matrix[j])) 
                 pass
@@ -66,16 +70,18 @@ def generate_paired_matrix(matrix):
     print(return_mat)
     print("# of rows: " + str(return_mat.shape[0]))
     print()
-    return return_mat
+    return idx_return, return_mat
 
 def main():
     inp = read_matrix_file("input.txt")
-    rand = rand_rows(128, inp)
-    mat = generate_paired_matrix(rand)
+    idx, rand = rand_rows(128, inp)
+    idx, mat = generate_paired_matrix(idx, rand)
     out = open('output.txt', 'w+')
     out.close()
-    print(minimum.min_row(mat))
-    print(minimum.min_row(mat).sum())
+    min_idx, min_mat = minimum.min_row(idx, mat)
+    print(min_mat)
+    print("Row idx: " + str(min_idx))
+    print(min_mat.sum())
     # with open('output.txt') as f:
     # 	for line in mat:
     #     	np.savetxt(f, line, fmt='%.18e')
--- minimum.py
+++ minimum.py
@@ -1,15 +1,21 @@
 import numpy as np
 
 # returns the row with the lowset sum in "mat"
-def min_row(mat):
+def min_row(idx, mat):
 	min_row = mat[0]
+	min_idx = idx[0]
 	#print("first row sum: " + str(min_row.sum()))
+	
+	count = 0
+
 	for row in mat:
 		temp = row.sum()
 		if temp < min_row.sum():
 			#print("new min: " + str(temp))
 			min_row = row
-	return min_row
+			min_idx = idx[count]
+		count += 1
+	return min_idx, min_row
 
 if __name__ == "__main__":
 	pass
